2ec5073d33f155489893d65e9f5c85eb16c10047
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2020-12-23 15:36:44 -0500

    moveX: move triangle by using translation matrix

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -4,13 +4,16 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <mat4x4.hpp>
+
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
 
 // Window dim
 const GLint WIDTH = 800, HEIGHT = 600;
 
 // ID
-GLuint VAO, VBO, shader, uniformXMove;
+GLuint VAO, VBO, shader, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -25,11 +28,11 @@ static const char* vShader = "                                        \n\
                                                                       \n\
 layout (location = 0) in vec3 pos;                                    \n\
                                                                       \n\
-uniform float xMove;                                                  \n\
+uniform mat4 model;                                                   \n\
                                                                       \n\
 void main()                                                           \n\
 {                                                                     \n\
-  gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);   \n\
+  gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);   \n\
 }";
 
 // Fragment Shader
@@ -134,7 +137,7 @@ void CompileShader() {
     }
 
     // Get uniform var location
-    uniformXMove = glGetUniformLocation(shader, "xMove");
+    uniformModel = glGetUniformLocation(shader, "model");
 }
 
 int main()
@@ -213,9 +216,13 @@ int main()
         // Draw triangle
         glUseProgram(shader);
 
-        // Set uniform var
+        // Identity matrix
+        glm::mat4 model(1.0f);
+        // Translation x value
+        model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
 
-        glUniform1f(uniformXMove, triOffset);
+        // Set uniform var
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
         glBindVertexArray(VAO);
 
